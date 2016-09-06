a730103843bbc82196674352c8116797e85c8878
btbujiangun <jiangjun4@sina.com>
2016-09-06 21:20:59 +0800

    compiled

--- src/utils/Logging.cpp
+++ src/utils/Logging.cpp
@@ -1,28 +1,135 @@
-##############################################
-# Author: Jun Jiang - jiangjun4@sina.com
-# Last modified:	2016-08-31 15:02
-# Filename:		Logging.cpp
-# Description: 
-##############################################
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Last modified:	2016-08-31 15:02
+* Filename:		Logging.cpp
+* Description: 
+*********************************************/
 
 #include "Logging.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "string.h"
+#include <vector>
 
 namespace ccml{
-void initialize_logging(int argc, char* argv[]){
-    (void)(argc);
-    if(!getenv("GLOG_logtostderr")){
-        google::LogToStderr();
+namespace logging{
+
+
+static std::string join(const std::string& part1, const std::string& part2){
+    const char sep = '/';
+    if(!part2.empty() && part2.front() == sep){
+        return part2;
     }
-    google::InstallFailureSignalHandler();
-    google::InitGoogleLogging(argv[0]);
+    std::string return_value;
+    return_value.reserve(part1.size() + part2.size() + 1);
+    return_value = part1;
+    if(!return_value.empty() && return_value.back() != sep){
+        return_value += sep;
+    }
+    return_value += part2;
+    return return_value;
 }
 
-namespace logging{
-    void install_failure_function(void (*callback)()){
-        google::InstallFailureFunction(callback);
+static inline bool env2bool(const char* env_name, bool default_value = false){
+    char* env_value = getenv(env_name);
+    if(env_value = nullptr){
+        return default_value;
+    }else{
+        return memchr("tTyY1\0", env_value[0], 6) != nullptr;
     }
-    void install_failure_writer(void (*callback)(const *char, int)){
-        google::InstallFailureWriter(callback);
+}
+static inline int env2int(const char* env_name, int default_value = 0){
+    char* env_value = getenv(env_name);
+    if(env_value == nullptr){
+        return default_value;
+    }else{
+        int return_value = default_value;
+        try{
+            return_value = std::stoi(env_value);
+        }catch(...){
+        //pass
+        }
+        return return_value;
     }
+}
+
+static inline int env2index(const char* env_name, const std::vector<std::string>& options, int default_value){
+    char* env_value = getenv(env_name);
+    if(env_value != nullptr){
+        for(size_t i = 0; i < options.size(); i++){
+            if(options[i] == env_value){
+                return static_cast<int>(i);
+            }
+        }
+    }
+    return default_value;
+}
+
+
+static bool g_log_inited = false;
+static bool g_log_to_stderr = env2bool("CC_LOG_LOGTOSTDERR", true);
+static const std::vector<std::string> g_log_level_name = {"INFO", "WARNING", "ERROR", "FATAL"};
+static int g_log_min_level = env2int("CC_LOG_MIN_LEVEL",env2index("CC_LOG_MIN_LEVEL", g_log_level_name, 0));
+
+static std::vector<std::vector<int>> g_log_fds;
+static std::vector<int> g_log_file_fds;
+
+static void (*g_failure_function_ptr)() __attribute__((noreturn)) = abort;
+
+static void free_log_file_fds(){
+    for(auto fd : g_log_file_fds){
+        close(fd);
+    }
+}
+static void initialize_log_fds(char* argc){
+    g_log_fds.resize(NUM_SEVERITIES);
+    for(int i = g_log_min_level; i < NUM_SEVERITIES && g_log_to_stderr; i++){
+        std::vector<int>& fds = g_log_fds[i];
+        fds.push_back(STDERR_FILENO);
+    }
+    char* log_dir = getenv("CC_LOG_LOGDIR");
+
+    for(int i = g_log_min_level; i < NUM_SEVERITIES && log_dir != nullptr; i++){
+        std::string file_name = join(log_dir, std::string(argc) + "." + g_log_level_name[i]);
+        int fd = open(file_name.c_str(), O_CREAT | O_WRONLY, 0644);
+        if( fd == -1){
+            fprintf(stderr, "Open log file error!");
+            exit(-1);
+        }
+        g_log_file_fds.push_back(fd);
+        std::vector<int>& current_fds = g_log_fds[i];
+        current_fds.insert(current_fds.end(), g_log_file_fds.begin(), g_log_file_fds.end());
+    }
+    atexit(free_log_file_fds);
+    g_log_inited = true;
+}
+
+
+LogMessage::LogMessage(const char* name, int line, int severity) : _name(name), _line(line), _severity(severity){
+}
+LogMessage::~LogMessage(){
+    this->generate_log_message();
+}
+void LogMessage::generate_log_message(){
+    if(!g_log_inited){
+        fprintf(stderr, "%c %s:%d] %s\n", "IWEF"[_severity], _name, _line, str().c_str());
+    }else{
+        for(auto& fd : g_log_fds[this->_severity]){
+            dprintf(fd, "%c %s:%d] %s\n", "IWEF"[_severity], _name, _line, str().c_str());
+        }
+    }
+}
+
+
+LogMessageFatal::LogMessageFatal(const char* file, int line):LogMessage(file, line, FATAL){
+}
+LogMessageFatal::~LogMessageFatal(){
+    generate_log_message();
+    g_failure_function_ptr();
+}
+
+
 }//namespace logging
 }//namespace ccml
--- src/utils/Logging.h
+++ src/utils/Logging.h
@@ -1,21 +1,77 @@
-##############################################
-# Author: Jun Jiang - jiangjun4@sina.com
-# Last modified:	2016-08-31 14:43
-# Filename:		Logging.h
-# Description:log header file 
-##############################################
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Last modified:	2016-08-31 14:43
+* Filename:		Logging.h
+* Description:log header file 
+*********************************************/
 
 #ifndef CCML_UTILS_LOGGING_H
 #define CCML_UTILS_LOGGING_H
 
-#include "glog/logging.h"
+#include <sstream>
 
 namespace ccml{
-void initialize_logging(int argc, char* argv[]);
 namespace logging{
-    void set_min_log_level(int level);
-    void install_failure_function(void (*callback)());
-    void install_failure_writer(void (*callback)(const char*,int));
+
+//Log Levels
+const int INFO = 0;
+const int WARNING = 1;
+const int ERROR = 2;
+const int FATAL = 3;
+const int NUM_SEVERITIES = 4;
+
+class LogMessage : public std::basic_ostringstream<char>{
+public:
+
+    LogMessage(const char* name, int line, int severity);
+    ~LogMessage();
+
+protected:
+    void generate_log_message();
+
+private:
+    const char* _name;
+    int _line;
+    int _severity;
+};//class LogMessage
+
+class LogMessageFatal : public LogMessage{
+public:
+    LogMessageFatal(const char* file, int line) __attribute__((cold));
+    ~LogMessageFatal() __attribute__((noreturn));
+};//class LogMessageFatal
+
+
+
+#define _CC_LOG_INFO \
+    ::ccml::logging::LogMessage(__FILE__, __LINE__, ::ccml::logging::INFO)
+
+#define _CC_LOG_WARNING \
+    ::ccml::logging::LogMessage(__FILE__, __LINE__, ::ccml::logging::WARNING)
+
+#define _CC_LOG_ERROR \
+    ::ccml::logging::LogMessage(__FILE__, __LINE__, ::ccml::logging::ERROR)
+
+#define _CC_LOG_FATAL \
+    ::ccml::logging::LogMessage(__FILE__, __LINE__, ::ccml::logging::FATAL)
+
+#define CC_LOG(severity) _CC_LOG_##severity
+
+#define PREDICT_TRUE(x) (__builtin__expect(!!(x), 1))
+#define PREDICT_FALSE(x) (__builtin_expect(x, 0))
+
+#define CC_CHECK(condition) \
+    if(PREDICT_FALSE(!(condition))) \
+    CC_LOG(FATAL) << "Check failed:" #condition " "
+
+#define CC_CHECK_EQ(v1, v2) CC_CHECK((v1) == (v2))
+#define CC_CHECK_NE(v1, v2) CC_CHECK((v1) != (v2))
+#define CC_CHECK_LE(v1, v2) CC_CHECK((v1) <= (v2))
+#define CC_CHECK_LT(v1, v2) CC_CHECK((v1) < (v2))
+#define CC_CHECK_GE(v1, v2) CC_CHECK((v1) >= (v2))
+#define CC_CHECK_GT(v1, v2) CC_CHECK((v1) > (v2))
+#define CC_CHECK_NOTNULL(v) CC_CHECK((v) != NULL)
+
 }//end logging
 }//end ccml
 #endif //CCML_UTILS_LOGGING_H
