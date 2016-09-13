d897725ba4c6084c32144a38890fc2f0a3918576
btbujiangun <jiangjun4@sina.com>
2016-09-13 20:09:30 +0800

    updated

---
+++ src/core/LightServer.cpp
@@ -0,0 +1,175 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2016-09-07 16:33
+* Last modified:	2016-09-13 16:09
+* Filename:		LightServer.cpp
+* Description: 
+**********************************************/
+
+#include "LightServer.h"
+#include "../utils/Logging.h"
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <linux/tcp.h>
+
+namespace ccml{
+namespace core{
+
+void SocketWorker::run(){
+    LOG(INFO) << "worker started, peer = " << _channel->get_peer_name();
+
+    std::vector<iovec> input_iovs;
+
+    while(true){
+        std::unique_ptr<MessageReader> message_reader = _channel->read_message();
+        if(!message_reader){
+            break;
+        }
+
+        auto callback = [this](const std::vector<iovec>& output_iovs){
+            _channel->write_message(output_iovs);
+    };
+
+        _server->handle_request(std::move(message_reader), callback);
+    }
+
+    LOG(INFO) << "worker begin to finish, peer = " << _channel->get_peer_name();
+}
+
+SocketServer::SocketServer(const std::string &addr, int port) : _addr(addr), _port(port){
+    _stopping = false;
+    _socket = 0;
+    _max_pending_connections = 100;
+}
+
+SocketServer::~SocketServer(){
+    _stopping = true;
+    {
+        SocketClient trigger(_addr.empty() ? "127.0.0.1": _addr, _port);
+    }
+    this->join();
+}
+void SocketServer::tcp_server(){
+    int newsockfd;
+    socklen_t client_len;
+    struct sockaddr_in server_addr, client_addr;
+    struct hostent* server;
+
+    _socket = socket(AF_INET, SOCK_STREAM, 0);
+    CC_CHECK(_socket >= 0) << "ERROR opening socket";
+
+    memset((char*)&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(_port);
+    if(!_addr.empty()){
+        server = gethostbyname(_addr.c_str());
+        CC_CHECK(server) << "ERROR, no such host: " << _addr;
+        bcopy((char*)server->h_addr, (char*)&server_addr.sin_addr.s_addr, server->h_length);
+    }else{
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+    }
+
+    set_option(_socket);
+
+    CC_CHECK(bind(_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) >= 0) << "ERROR on binding " <<_addr;
+
+    listen(_socket, _max_pending_connections);
+    client_len = sizeof(client_addr);
+
+    while(true){
+        newsockfd = accept(_socket, (struct sockaddr*)&client_addr, &client_len);
+        if(_stopping){
+            break;
+        }
+        CC_CHECK(newsockfd >=0 ) << "ERROR on accept.";
+        constexpr int k_peer_name_len = 128;
+        char peer_name[k_peer_name_len];
+        CC_CHECK(inet_ntop(AF_INET, &client_addr.sin_addr, peer_name, k_peer_name_len));
+
+        SocketWorker* worker = new SocketWorker(create_channel(newsockfd, std::string(peer_name)), this);
+        worker->start();
+        worker->detach();
+    }
+    close(_socket);
+    LOG(INFO) << "LightServer accept thread finished, addr=" << _addr << " port=" << _port;
+}
+
+void SocketServer::run(){
+    LOG(INFO) << "tcp server started.";
+    tcp_server();
+}
+
+
+SocketClient::SocketClient(const std::string &server_addr, int server_port){
+    struct sockaddr_in serv_addr;
+    struct hostent hostinfo, *server;
+    char buf[1024];
+    int error_return;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    CC_CHECK(sockfd >= 0) << "ERROR opening socket.";
+    CC_CHECK_EQ(0, gethostbyname_r(server_addr.c_str(), &hostinfo, buf, sizeof(buf), &server, &error_return)) << "ERROR, no such host: " << server_addr << " ret= " << error_return;
+    CC_CHECK(server) << "gethostbyname_r error.";
+
+    memset((char*)&server_addr, 0, sizeof(server_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(server_port);
+
+    set_option(sockfd);
+
+    CC_CHECK(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) >= 0) << "ERROR connecting to " << server_addr;
+    _channel.reset(new SocketChannel(sockfd, server_addr));
+}
+
+
+void set_option(int sockfd){
+    //int send_size = FLAGS_sock_send_buf_size;
+    //int recv_size = FLAGS_sock_recv_buf_size;
+
+    int send_size = 1024 * 1024 * 40;
+    int recv_size = 1024 * 1024 * 40;
+    bool FLAGS_small_messages = false;
+
+    CC_CHECK_GE(setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &recv_size, sizeof(recv_size)), 0);
+    CC_CHECK_GE(setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &recv_size, sizeof(recv_size)), 0);
+    CC_CHECK_GE(setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &send_size, sizeof(send_size)), 0);
+
+    if(FLAGS_small_messages){
+        int optval = 1;
+        CC_CHECK_GE(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval)), 0);
+        optval = 1;
+        CC_CHECK_GE(setsockopt(sockfd, IPPROTO_TCP, TCP_QUICKACK, &optval, sizeof(optval)), 0);
+    }
+    int reuse = 1;
+    CC_CHECK_GE(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)), 0);
+}
+
+std::string get_ip_addr(std::string &device){
+    int sockfd;
+    struct sockaddr_in sin;
+    struct ifreq ifr;
+    
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    CC_CHECK(sockfd >= 0) << "Create socket error.";
+
+    strncpy(ifr.ifr_name, device.c_str(), IFNAMSIZ);
+    ifr.ifr_name[IFNAMSIZ - 1] = 0;
+
+    CC_CHECK_GE(ioctl(sockfd, SIOCGIFADDR, &ifr), 0);
+    memcpy(&sin, &ifr.ifr_addr, sizeof(sin));
+    close(sockfd);
+
+    return std::string(inet_ntoa(sin.sin_addr));
+}
+
+}//namespace core
+}//namesapce ccml
---
+++ src/core/SocketChannel.cpp
@@ -0,0 +1,159 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2016-09-08 16:10
+* Last modified:	2016-09-09 17:39
+* Filename:		SocketChannel.cpp
+* Description: 
+**********************************************/
+
+#include "SocketChannel.h"
+#include <unistd.h>
+
+namespace ccml{
+namespace core{
+
+SocketChannel::~SocketChannel(){
+    close(_tcp_socket);
+    LOG(INFO) << "destory connect in socket channel, peer = " << _peer_name;
+}
+
+size_t SocketChannel::read(void* buf, size_t size){
+    size_t total = 0;
+    while(total < size){
+        ssize_t length = ::read(_tcp_socket, (char*)buf+total, size-total);
+        CC_CHECK(length >= 0) << " peer = " << _peer_name;
+        if(length <= 0){
+            return total;
+        }
+        total += length;
+    }
+    return total;
+}
+
+size_t SocketChannel::write(const void* buf, size_t size){
+    size_t total = 0;
+    while(total < size){
+        ssize_t length = ::write(_tcp_socket, (const char*)buf + total, size - total);
+        CC_CHECK(length >= 0) << " peer = " << _peer_name;
+        if(length <= 0){
+            return total;
+        }
+        total += length;
+    }
+    return total;
+}
+
+templete<class IOFUNC, class SOCKET>
+static size_t readwritev(IOFUNC io_func, SOCKET socket, iovec* iovs, int num_iovs, int max_iovs, const std::string& peer_name){
+    int current_iov = 0;
+    size_t total = 0;
+
+    for(int i = 0; i < num_iovs; i++){
+        total += iovs[i].iov_len;
+    }
+
+    size_t size = 0;
+    size_t current_iov_size_done = 0;
+
+    while(size < total){
+        ssize_t length = iofunc(socket, &iovs[current_iov], std::min(num_iovs- current_iov, maxiovs));
+        CC_CHECK(length > 0) << " peer = " << _peer_name << " current_iov = " << current_iov << " num_iov = " << num_iovs << " iovs[current_iov].base = " << iovs[current_iov].iov_base << " iovs[current_iov].iov_len = " << iovs[current_iov].iov_len;
+        size += length;
+
+        iovs[current_iov].iov_base = (void*)((char*)iovs[current_iov].iov_base - current_iov_size_done);
+
+        iovs[current_iov].iov_len += current_iov_size_done;
+        length += current_iov_size_done;
+
+        while(current_iov < num_iovs){
+            if((size_t)length < iovs[current_iov].iov_len){
+                break;
+            }
+            length -= iovs[current_iov].iov_len;
+            ++current_iov;
+        }
+        if(current_iov < num_iovs){
+            current_iov_size_done = length;
+            iovs[current_iov].iov_base = (void*)((char*)iovs[current_iov].iov_base + length);
+            iovs[current_iov].iov_len -= length;
+        }
+    }
+    return size;
+}
+
+size_t SocketChannel::readv(std::vector<struct iovec>* iovs){
+    return readwritev(::readv, _tcp_socket, const_cast<iovec*>(&(*iovs)[0]), iovs->size(), UIO_MAXIOV, _peer_name);
+}
+
+size_t SocketChannel::writev(const std::vector<struct iovec>& iovs){
+    return readwritev(::writev, _tcp_socket, const_cast<iovec*>(&iovs[0]),iovs.size(), UIO_MAXIOV, _peer_name);
+}
+
+std::unique_ptr<MessageReader> SocketChannel::read_message(){
+    MessageHeader header;
+    size_t length = read(&header,sizeof(header));
+    if(length == 0){
+        return nullptr;
+    }
+
+    CC_CHECK(length == sizeof(header));
+
+    std::unique_ptr<MessageReader> message_reader(new MessageReader(this, header.num_iovs));
+    CC_CHECK_QE(message_reader->get_total_length() + sizeof(header) + message_reader->get_num_blocks() * sizeof(size_t), (size_t)header.total_length) << " total_length = " << message_reader->get_total_length() << " num_blocks = " << message_reader->get_num_blocks();
+    return message_reader;
+}
+
+void SocketChannel::write_message(const std::vector<struct iovec>& user_iovs){
+    MessageHeader header;
+    header.num_iovs = user_iovs.size();
+
+    std::vector<size_t> iov_lengths;
+    iov_lengths.reverse(user_iovs.size());
+    for(auto& iov : user_iovs){
+        iov_lengths.push_backs(iov.iov_len);
+    }
+
+    std::vector<iovec> iovs;
+    iovs.reserve(user_iovs.size()+2);
+    iovs.push_back({&header, sizeof(header)});
+    iovs.push_back({&iov_lengths[0], sizeof(iov_lengths[0]) * header.num_iovs});
+    iovs.insert(iovs.end(), user_iovs.begin(), user_iovs.end());
+
+    header.total_length = 0;
+    for(auto& iov : iovs){
+        header.total_length += iov.iov_len;
+    }
+
+    CC_CHECK(writev(iovs) == (size_t)header.total_length);
+}//end SocketChannel
+
+
+MessageReader::MessageReader(SocketChannel* channel, size_t num_blocks) : _channel(channel), _block_lengths(num_blocks), _current_block_index(0) {
+    size_t size = num_blocks * sizeof(_num_blocks[0]);
+    CC_CHECK(_channel->read(&_num_blocks[0], size) == size);
+}
+
+void MessageReader::read_blocks(const std::vector<void*>& bufs){
+    CC_CHECK_LE(_current_block_index + bufs.size(), _num_blocks.size());
+
+    std::vector<iovec> iovs;
+    iovs.reserve(bufs.size());
+    size_t total = 0;
+    for(void* buf :bufs){
+        iovs.push_back({buf, get_next_block_length()});
+        total += get_next_block_length();
+        ++_current_block_index;
+    }
+
+    CC_CHECK(_channel->readv(&iovs) == total);
+}
+
+void MessageReader::read_next_block(void* buf){
+    CC_CHECK_LT(_current_block_index, _num_blocks.size());
+    CC_CHECK(_channel->read(buf, get_next_block_length()) == get_next_block_length());
+    ++current_block_index;
+}
+
+
+}//namespace core
+}//namespace ccml
