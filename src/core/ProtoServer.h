ffad17c77b204c62d491ae902ca77c0190d2de6d
btbujiangun <jiangjun4@sina.com>
2016-09-14 17:55:39 +0800

    updated

--- src/core/ProtoServer.h
+++ src/core/ProtoServer.h
@@ -1,31 +1,36 @@
 /*********************************************
 * Author: Jun Jiang - jiangjun4@sina.com
 * Created: 2016-09-13 16:09
-* Last modified: 2016-09-13 16:09
-* Filename: ProtoServer.h
+* Last modified:	2016-09-14 14:40
+* Filename:		ProtoServer.h
 * Description: 
 **********************************************/
 
 #ifndef CCML_CORE_PROTOSERVER_H
 #define CCML_CORE_PROTOSERVER_H
 
+#include <map>
 #include "LightServer.h"
+#include "ThreadLocal.h"
+#include "SocketChannel.h"
 #include <google/protobuf/message_lite.h>
 
 namespace ccml{
 namespace core{
-class ProtoServer : public SocketSever{
+class ProtoServer : public SocketServer{
 public:
     ProtoServer(const std::string& addr, int port) : SocketServer(addr, port){
     }
 
     typedef std::function<void(const google::protobuf::MessageLite& proto_out)> ProtoResponseCallback;
-    typedef std::function<void(const google::protobuf::MessageList& proto_out, const std::vector<iovec>& output_iovs)> ProtoResponseCallbackException;
-
     template <class T>
-    void register_service_function_exception(\
+    void register_service_function(const std::string& function_name, std::function<void(const T& request, ProtoResponseCallback callback)> function);
+
+    typedef std::function<void(const google::protobuf::MessageLite& proto_out, const std::vector<iovec>& output_iovs)> ProtoResponseCallbackEx;
+    template <class T>
+    void register_service_function_ex(\
             const std::string& function_name, \
-            std::function<void(const T&, std::unique_ptr<MessageReader> messager_reader, ProtoResponseCallbackException callback)> function);
+            std::function<void(const T&, std::unique_ptr<MessageReader> messager_reader, ProtoResponseCallbackEx callback)> function);
 
 protected:
     virtual void handle_request(std::unique_ptr<MessageReader> message_reader, ResponseCallback callback);
@@ -49,7 +54,7 @@ public:
 
     void send(const char* function_name, const google::protobuf::MessageLite& proto, const std::vector<iovec>& ivo = std::vector<iovec>());
 
-    std::unique_ptr<MessageReader> recv(google::protobuff::MessageLite* proto);
+    std::unique_ptr<MessageReader> recv(google::protobuf::MessageLite* proto);
 
     std::unique_ptr<MessageReader> send_recv(const char* function_name, const google::protobuf::MessageLite& proto_in, google::protobuf::MessageLite* proto_out){
         send(function_name, proto_in);
@@ -63,7 +68,46 @@ public:
 };//class ProtoClient
 
 
+template<class T>
+void ProtoServer::register_service_function_ex(const std::string& function_name, std::function<void(const T&, std::unique_ptr<MessageReader> message_reader, ProtoResponseCallbackEx callback)> func){
+    auto f = [func](std::unique_ptr<MessageReader> message_reader, ResponseCallback callback){
+        T request;
+        std::string str(message_reader->get_next_block_length(), 0);
+        message_reader->read_next_block(&str[0]);
+        CC_CHECK(request.ParseFromString(str));
+        auto pcob = [callback](const google::protobuf::MessageLite& response, const std::vector<iovec>& output_iovs){
+            std::string out;
+            CC_CHECK(response.SerializeToString(&out));
+            std::vector<iovec> iovs;
+            iovs.push_back({&out[0], out.size()});
+            iovs.insert(iovs.end(), output_iovs.begin(), output_iovs.end());
+            callback(iovs);
+        };
+        func(request, std::move(message_reader), pcob);
+    };
+    register_service_function_implement(function_name, f);
+}
 
+template <class T>
+void ProtoServer::register_service_function(const std::string& function_name, std::function<void(const T&, ProtoResponseCallback callback)> func){
+    auto f = [func](std::unique_ptr<MessageReader> message_reader, ResponseCallback callback){
+        T request;
+        std::string str(message_reader->get_next_block_length(), 0);
+        message_reader->read_next_block(&str[0]);
+        CC_CHECK(request.ParseFromString(str));
+        message_reader.reset();
+
+        auto pcob = [callback](const google::protobuf::MessageLite& response){
+            std::string out;
+            CC_CHECK(response.SerializeToString(&out));
+            std::vector<iovec> iovs;
+            iovs.push_back({&out[0], out.size()});
+            callback(iovs);
+        };
+        func(request, pcob);
+    };
+    register_service_function_implement(function_name, f);
+}
 
 }//namespace core
 }//namespace ccml
---
+++ src/core/ThreadLocal.h
@@ -0,0 +1,57 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2016-09-14 15:53
+* Last modified: 2016-09-14 15:53
+* Filename: ThreadLocal.h
+* Description: 
+**********************************************/
+
+#ifndef CCML_CORE_THREADLOCAL_H
+#define CCML_CORE_THREADLOCAL_H
+
+namespace ccml{
+namespace core{
+template <class T>
+class ThreadLocal{
+public:
+    ThreadLocal(){
+        /// param2 clear func
+        CC_CHECK(pthread_key_create(&_thread_specific_key, data_destructor) == 0);
+    }
+    ~ThreadLocal(){
+        pthread_key_delete(_thread_specific_key);
+    }
+
+    T* get(bool create_local = true){
+        T* p = (T*)pthread_getspecific(_thread_specific_key);
+        if(!p && create_local){
+            p = new T();
+            CC_CHECK(pthread_setspecific(_thread_specific_key, p) == 0);
+        }
+        return p;
+    }
+
+    void set(T* p){
+        if(T* q = get(false)){
+            data_destructor(q);
+        }
+        CC_CHECK(pthread_setspecific(_thread_specific_key, p) == 0);
+    }
+
+    T& operator*(){
+        return *get();
+    }
+
+    operator T*(){
+        return get();
+    }
+
+private:
+    static void data_destructor(void* p){
+        delete (T*)p;
+    }
+    pthread_key_t _thread_specific_key;
+};//class ThreadLocal
+}//namespace core
+}//namespace ccml
+#endif
