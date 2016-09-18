86be024fa420bcd8e2a8f977ad065cc9bbdd7ac4
btbujiangun <jiangjun4@sina.com>
2016-09-18 19:54:55 +0800

    update

---
+++ src/core/ParameterServer.h
@@ -0,0 +1,21 @@
+/*********************************************
+* Author: Jun Jiang - jiangjun4@sina.com
+* Created: 2016-09-18 19:47
+* Last modified: 2016-09-18 19:47
+* Filename: ParameterServer.h
+* Description: 
+**********************************************/
+
+#ifndef CCML_CORE_PARAMETERSERVER_H
+#define CCML_CORE_PARAMETERSERVER_H
+
+#include "ProtoServer.h"
+
+namespace ccml{
+namespace core{
+class ParameterServer : public ProtoServer{
+};//class ParameterServer
+}//namespace core
+}//namespace ccml
+
+#endif
