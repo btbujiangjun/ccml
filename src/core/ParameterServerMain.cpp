937654f556ef0ec41eb31c46cdae527befbb2a1a
btbujiangun <jiangjun4@sina.com>
2016-10-09 18:31:26 +0800

    update

--- src/core/ParameterServerMain.cpp
+++ src/core/ParameterServerMain.cpp
@@ -6,17 +6,46 @@
 * Description: 
 **********************************************/
 
+#include "ParameterServer.h"
 
 using namespace ccml::core;
 using namespace ccml::utils;
 
 int main(int argc, char** argv){
+
     /// todo
     ///initmain()
-    std::vector<std::string> devices;
     std::vector<std::shared_ptr<ParameterServer>> pservers;
 
+    int num_ports = FLAGS_num_ports + FLAGS_num_ports_for_sparse;
 
+    if(FLAGS_nics.empty()){
+
+        pservers.resize(num_ports);
+
+        for(int i = 0; i < num_ports; i++){
+            pservers[i].reset(new ParameterServer(std::string(), FlAGS_port + i));
+            CC_CHECK(pservers[i]->init()) << "Fail to initialize parameter server " << FLAGS_port + i;
+            LOG(INFO) << "pserver started: " << FLAGS_port + i;
+            pservers[i]->start();
+        }
+
+    }else{
+
+        std::vector<std::string> devices;
+        str::split(FLAGS_nics, ',', &devices);
+        pservers.resize(devices.size() * num_ports);
+
+        for(int i = 0; i < num_ports; i++){
+            for(size_t j = 0; j < devices.size(); j++){
+                pservers[i * devices() + j].reset(new PrameterServer(getIpAddr(devices[i]), FLAGS_port + i));
+                CC_CHECK(pservers[i * devices.size() + j]->init()) << "Fail to initialize parameter server " << devices[j] << FLAG_port + i;
+                LOG(INFO) << "pserver started:" << devices[j] << ":" << FLAGS_port + i;
+                pservers[i * devices.size() + j]->start();
+            }
+        }
+
+    }
 
     for(auto& pserver: pservers){
         pserver->join();
