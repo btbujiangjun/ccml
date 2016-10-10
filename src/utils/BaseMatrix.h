8585ea7bf89b68ce651e8c2ea4400b0e58b9b369
btbujiangun <jiangjun4@sina.com>
2016-10-10 15:22:03 +0800

    delete file

--- src/utils/BaseMatrix.h
+++
@@ -1,8 +0,0 @@
-/*********************************************
-* Author: Jun Jiang - jiangjun4@sina.com
-* Created: 2016-09-20 12:02
-* Last modified: 2016-09-20 12:02
-* Filename: BaseMatrix.h
-* Description: 
-**********************************************/
-
